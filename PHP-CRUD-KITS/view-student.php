47ce82f72229207bb556742dc9bfcb9070b6fae1
balasrinivasanammu <balajiit88@gmail.com>
2023-12-15 11:10:42 +0530

    Add files via upload

--- PHP-CRUD-KITS/view-student.php
+++ PHP-CRUD-KITS/view-student.php
@@ -1,4 +1,50 @@
-//view-student.php
+//view-student.php.
 <?php
-echo "welcome to view page";
+include "dbconfig.php";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Database</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+        <h2>Student Details</h2>
+<table class="table">
+    <thead>
+        <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Email</th>
+        <th>Action</th>
+
+    </tr>
+    </thead>
+    <tbody>
+        <?php
+                $sql = "SELECT * FROM customer";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+        ?>
+                    <tr>
+                    <td><?php echo $row['cid']; ?></td>
+                    <td><?php echo $row['cname']; ?></td>
+                    <td><?php echo $row['age']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><a class="btn btn-info" href="update-student.php?id=<?php echo $row['cid']; ?>">Edit</a>
+                     &nbsp;
+                     <a class="btn btn-danger" href="delete-student.php?id=<?php echo $row['cid']; ?>">Delete</a>
+                    </td>
+                    </tr>
+        <?php       }
+            }
+        ?>
+    </tbody>
+</table>
+    </div>
+</body>
+</html>
